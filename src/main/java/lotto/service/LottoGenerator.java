3ca1011f90c9da25d6e631e1ffb99fea48d0b1d1
hajimeong <ha_01@naver.com>
2025-10-29 21:59:09 +0900

    feat(LottoGenerator.java): 랜덤 로또 생성 기능 구현

---
+++ src/main/java/lotto/service/LottoGenerator.java
@@ -0,0 +1,32 @@
+package lotto.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.domain.Lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoGenerator {
+    private static final int LOTTO_PRICE=1000;
+
+    //금액에 맞는 수량의 로또 생성
+    public List<Lotto> generator(int price){
+        int amount = calculateAmount(price);
+        List<Lotto>lottos=new ArrayList<Lotto>();
+
+        for(int i=0;i<amount;i++){
+            lottos.add(generateSingleLotto());
+        }
+        return lottos;
+    }
+
+    private int calculateAmount(int price){
+        return price/LOTTO_PRICE;
+    }
+
+    //하나의 로또 번호 생성 -> 랜덤
+    private Lotto generateSingleLotto(){
+        List<Integer> numbers= Randoms.pickUniqueNumbersInRange(1,45,6);
+        return new Lotto(numbers);
+    }
+}
