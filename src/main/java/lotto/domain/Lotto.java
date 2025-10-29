eb9f698888f8038422b0ab297187bc32df1f8050
hajimeong <ha_01@naver.com>
2025-10-29 22:45:21 +0900

    feat(Lotto.java): Lotto 클래스에 getter 및, 중복 검증 메서드 및 toString 오버라이딩 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,12 +1,16 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
     }
 
@@ -16,4 +20,22 @@ public class Lotto {
         }
     }
 
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    private static void validateDuplicate(List<Integer>numbers){
+        Set<Integer> set=new HashSet<>(numbers);
+        if(set.size()!=numbers.size()){
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복되는 숫자가 존재합니다.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return numbers.stream()
+                .sorted()
+                .map(String::valueOf)
+                .collect(Collectors.joining(", ","[","]"));
+    }
 }
