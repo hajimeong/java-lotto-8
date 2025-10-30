bcac4b6230a40cbc61243b31eab768b8b8acdf8f
hajimeong <ha_01@naver.com>
2025-10-30 22:48:42 +0900

    feat(Lotto.java): 로또번호 범위 검증 로직 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -6,6 +6,8 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Lotto {
+    private static final int MIN_NUMBER=1;
+    private static final int MAX_NUMBER=45;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -28,6 +30,15 @@ public class Lotto {
         Set<Integer> set=new HashSet<>(numbers);
         if(set.size()!=numbers.size()){
             throw new IllegalArgumentException("[ERROR] 로또 번호에 중복되는 숫자가 존재합니다.");
+        }
+    }
+
+    //로또 번호가 범위를 벗어났는지 검증
+    private static void validateRange(List<Integer>numbers){
+        for(Integer num:numbers){
+            if(num<MIN_NUMBER||num>MAX_NUMBER){
+                throw new IllegalArgumentException("[ERROR] 로또 번호가 범위를 벗어났습니다.(1~45)");
+            }
         }
     }
 
