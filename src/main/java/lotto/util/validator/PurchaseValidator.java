e5d9aa95651e85520fb59fdf682c15dfd38c6ebc
hajimeong <ha_01@naver.com>
2025-10-31 00:46:48 +0900

    feat(PurchaseValidator.java): 구입금액 단위 검증 구현

---
+++ src/main/java/lotto/util/validator/PurchaseValidator.java
@@ -0,0 +1,16 @@
+package lotto.util.validator;
+
+public class PurchaseValidator {
+    private static final int LOTTO_PRICE=1000;
+
+    //구입금액이 1000원 단위인지 검증
+    public void validatePurchase(int purchasePrice){
+        validateUnit(purchasePrice);
+    }
+
+    private static void validateUnit(int amount){
+        if(amount%LOTTO_PRICE!=0){
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 단위여야 합니다.");
+        }
+    }
+}
