8cb08e6879908e7a20e024125deefd08cd84a2de
hajimeong <ha_01@naver.com>
2025-11-02 21:55:36 +0900

    feat(LottoRank.java): 로또 당첨 등수 판별 enum 생성

---
+++ src/main/java/lotto/domain/LottoRank.java
@@ -0,0 +1,34 @@
+package lotto.domain;
+
+//로또의 일치 개수와 보너스 일치 여부에 따른 당첨 등수를 판별하는 클래스
+public enum LottoRank {
+    FIRST(6,false,2000000000),
+    SECOND(5,true, 30000000),
+    THIRD(5, false, 1500000),
+    FOURTH(4, false, 50000),
+    FIFTH(3,false,5000),
+    MISS(0, false,0);
+
+    private final int matchCount; //맞은 개수
+    private final boolean matchBonus; //보너스 맞춤 유무
+    private final int prize; //상금
+
+    LottoRank(int matchCount, boolean matchBonus, int prize) {
+        this.matchCount = matchCount;
+        this.matchBonus = matchBonus;
+        this.prize = prize;
+    }
+
+    public static LottoRank result(int matchCount, boolean matchBonus) {
+        if(matchCount==6)return FIRST;
+        if(matchCount==5&& matchBonus)return SECOND;
+        if(matchCount==5)return THIRD;
+        if(matchCount==4)return  FOURTH;
+        if(matchCount==3)return  FIFTH;
+        return MISS;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+}
