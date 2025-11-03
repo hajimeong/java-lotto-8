f9ff846beecd04847677eeee9d46deef6b681606
hajimeong <ha_01@naver.com>
2025-11-03 17:37:00 +0900

    refector(LottoRank.java): Enum 상수를 낮은 순위에서 높은 순위 순으로 재정렬

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -2,12 +2,12 @@ package lotto.domain;
 
 //로또의 일치 개수와 보너스 일치 여부에 따른 당첨 등수를 판별하는 클래스
 public enum LottoRank {
-    FIRST(6,false,2000000000),
-    SECOND(5,true, 30000000),
-    THIRD(5, false, 1500000),
-    FOURTH(4, false, 50000),
+    MISS(0, false,0),
     FIFTH(3,false,5000),
-    MISS(0, false,0);
+    FOURTH(4, false, 50000),
+    THIRD(5, false, 1500000),
+    SECOND(5,true, 30000000),
+    FIRST(6,false,2000000000);
 
     private final int matchCount; //맞은 개수
     private final boolean matchBonus; //보너스 맞춤 유무
